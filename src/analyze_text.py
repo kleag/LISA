f4c0c05f6000f5e5a56c5bfc3c67941856813959
Gael de Chalendar <gael.de-chalendar@cea.fr>
2020-05-26 17:31:51 +0200

    test

--- src/analyze_text.py
+++ src/analyze_text.py
@@ -242,7 +242,7 @@ with tf.Session() as sess:
           if len(token.text) > 0 and token.text != '\n':
           #0:domain  1:sent_id 2:id  3:word+word_type  4:gold_pos    5:auto_pos    6:parse_head  7:parse_label _
 
-            line = f'conll05\t{sentence_id}\t{token_id}\t{token.text}\t{token.pos_}\t{token.tag_}\t{tokens[token.head]}\t{token.dep_}\n')
+            line = f'conll05\t{sentence_id}\t{token_id}\t{token.text}\t{token.pos_}\t{token.tag_}\t{tokens[token.head]}\t{token.dep_}\t-\t-\t-\t-\t-\t-\t-\t-\n')
             #line = f'conll05\t{sentence_id}\t{token_id}\t{token.text}\t_\t_\t_\t_\n'
             #line = f'{token_id}\t{token.text}\t_\t_\t_\t_\t_\t_\n'
             tf.logging.log(tf.logging.INFO, f"Writing to temp file {temp.name}: {line}")
